047095d74b70228f465f9b7ec04d196d513d2385
harsh <harshaggarwal@Harshs-MacBook-Pro.local>
2020-06-23 12:26:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tataskmobileapp2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tataskmobileapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(
+                        FlutterActivity.createDefaultIntent(MainActivity.this)
+                );
+            }
+        });
+    }
+}
